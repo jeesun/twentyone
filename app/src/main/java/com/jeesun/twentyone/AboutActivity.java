fa31adb5729cfb0aced9e2cee2ae82993fa357d6
SimonSun <simon.sun.dev@hotmail.com>
2017-12-09 21:28:23 +0800

    更新“关于”界面；解耦。

--- app/src/main/java/com/jeesun/twentyone/AboutActivity.java
+++ app/src/main/java/com/jeesun/twentyone/AboutActivity.java
@@ -1,13 +1,35 @@
 package com.jeesun.twentyone;
 
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    TextView tvGithub;
+
+    String versionName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        setTitle(R.string.about);
+
+        tvGithub = findViewById(R.id.github);
+
+        try {
+            versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("version: " + versionName + "\n");
+        sb.append("author: jeesun\n");
+        sb.append("github: ");
+        sb.append(getString(R.string.github));
+        tvGithub.setText(sb);
     }
 }
