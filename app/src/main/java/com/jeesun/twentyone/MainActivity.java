300539bb1582e0950d51305b43d97edba7609357
SimonSun <simon.sun.dev@hotmail.com>
2017-12-09 17:30:44 +0800

    裁剪的图片存储到sdcard下的TwentyOne文件夹下。

--- app/src/main/java/com/jeesun/twentyone/MainActivity.java
+++ app/src/main/java/com/jeesun/twentyone/MainActivity.java
@@ -1,10 +1,16 @@
 package com.jeesun.twentyone;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,6 +25,7 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     public final static int REQUEST_IMAGE_CAPTURE = 1;
+    public final static int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 2;
     public final static String IMAGE_TYPE = "image/*";
 
     Button btnAlbum;
@@ -39,11 +46,52 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
             }
         });
+
+        // 版本判断。当手机系统大于 23 时，才有必要去判断权限是否获取
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // 检查该权限是否已经获取
+            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                //权限未获取
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    //第一次全新进入时，shouldShowRequestPermissionRationale方法将返回false,这里将会执行。
+                    //请求权限时如果点了拒绝但是没勾选不再提醒，shouldShowRequestPermissionRationale方法将返回true，这里将不执行。
+                    //点了拒绝且勾选了不再提醒，再次进入时，shouldShowRequestPermissionRationale方法也将返回false,并且权限请求将无任何响应，然后可以在下面方法中做些处理，提示用户打开权限。
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+                }
+            }else{
+                //权限已获取
+                //创建文件夹用于保存裁剪的图片
+                String dirPath = Environment.getExternalStorageDirectory().getPath() + "/TwentyOne";
+                File file = new File(dirPath);
+                if(!file.exists()){
+                    file.mkdirs();
+                }
+            }
+        }else{
+            //android版本低于23，权限已获取
+            //创建文件夹用于保存裁剪的图片
+            String dirPath = Environment.getExternalStorageDirectory().getPath() + "/TwentyOne";
+            File file = new File(dirPath);
+            if(!file.exists()){
+                file.mkdirs();
+            }
+        }
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if(resultCode == RESULT_OK){
             if(requestCode == REQUEST_IMAGE_CAPTURE){
                 //Toast.makeText(this, "获取相册", Toast.LENGTH_SHORT).show();
@@ -56,9 +104,11 @@ public class MainActivity extends AppCompatActivity {
             if (requestCode == UCrop.REQUEST_CROP) {
                 Uri croppedFileUri = UCrop.getOutput(data);
                 //获取默认的下载目录
-                String downloadsDirectoryPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+                //String downloadsDirectoryPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+                String dirPath = Environment.getExternalStorageDirectory().getPath() + "/TwentyOne";
                 String filename = String.format("%d_%s", Calendar.getInstance().getTimeInMillis(), croppedFileUri.getLastPathSegment());
-                File saveFile = new File(downloadsDirectoryPath, filename);
+                Log.i("downloadsDirectoryPath", dirPath);
+                File saveFile = new File(dirPath, filename);
                 //保存下载的图片
                 FileInputStream inStream = null;
                 FileOutputStream outStream = null;
@@ -88,6 +138,35 @@ public class MainActivity extends AppCompatActivity {
         //裁切失败
         if (resultCode == UCrop.RESULT_ERROR) {
             Toast.makeText(this, "裁切图片失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "已授予读写手机存储权限", Toast.LENGTH_SHORT).show();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    //创建文件夹用于保存裁剪的图片
+                    String dirPath = Environment.getExternalStorageDirectory().getPath() + "/TwentyOne";
+                    File file = new File(dirPath);
+                    if(!file.exists()){
+                        file.mkdirs();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "拒绝授予读写手机存储权限将无法获取相册图片，请重新授权", Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
     }
 
