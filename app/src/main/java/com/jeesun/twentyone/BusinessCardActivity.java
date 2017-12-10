b5840a2fa2f58e235900611f3d882f78a32a8e33
SimonSun <simon.sun.dev@hotmail.com>
2017-12-10 19:08:31 +0800

    名片新增更换背景图和设置字体大小功能。

--- app/src/main/java/com/jeesun/twentyone/BusinessCardActivity.java
+++ app/src/main/java/com/jeesun/twentyone/BusinessCardActivity.java
@@ -1,10 +1,12 @@
 package com.jeesun.twentyone;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -25,7 +27,7 @@ public class BusinessCardActivity extends AppCompatActivity {
     private static final String TAG = BusinessCardActivity.class.getName();
     private ImageView mSourImage;
     private ImageView mWartermarkImage;
-    private EditText etLeftTop, etLeftBottom, etRightTop, etRightBottom, etCenter;
+    private EditText etFontSize, etLeftTop, etLeftBottom, etRightTop, etRightBottom, etCenter;
     private Button btnMake, btnSave;
     private static final int padding = 12;
 
@@ -45,6 +47,7 @@ public class BusinessCardActivity extends AppCompatActivity {
     private void initView(){
         mSourImage =  findViewById(R.id.sour_pic);
         mWartermarkImage =  findViewById(R.id.wartermark_pic);
+        etFontSize = findViewById(R.id.font_size);
         etLeftTop = findViewById(R.id.left_top);
         etLeftBottom = findViewById(R.id.left_bottom);
         etRightTop = findViewById(R.id.right_top);
@@ -55,6 +58,17 @@ public class BusinessCardActivity extends AppCompatActivity {
 
         Bitmap sourBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bg_business_card);
         mSourImage.setImageBitmap(sourBitmap);
+
+
+        mSourImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, null);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, 0x1);
+            }
+        });
+
 
         //Bitmap waterBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.weixin);
 
@@ -67,13 +81,26 @@ public class BusinessCardActivity extends AppCompatActivity {
         btnMake.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Bitmap sourBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bg_business_card);
+                mSourImage.setDrawingCacheEnabled(true);
+                //Bitmap sourBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bg_business_card);
+                Bitmap sourBitmap = mSourImage.getDrawingCache();
 
-                Bitmap textBitmap = ImageUtil.drawTextToLeftTop(BusinessCardActivity.this, sourBitmap, etLeftTop.getText().toString(), 36, "fonts/方正魏碑简体.ttf", Color.BLACK, padding, padding);
-                textBitmap = ImageUtil.drawTextToRightBottom(BusinessCardActivity.this, textBitmap, etRightBottom.getText().toString(), 36, "fonts/方正魏碑简体.ttf", Color.BLACK, padding, padding);
-                textBitmap = ImageUtil.drawTextToRightTop(BusinessCardActivity.this, textBitmap, etRightTop.getText().toString(), 36,"fonts/方正魏碑简体.ttf", Color.BLACK, padding, padding);
-                textBitmap = ImageUtil.drawTextToLeftBottom(BusinessCardActivity.this, textBitmap, etLeftBottom.getText().toString(), 36, "fonts/方正魏碑简体.ttf", Color.BLACK, padding, padding);
-                textBitmap = ImageUtil.drawTextToCenter(BusinessCardActivity.this, textBitmap, etCenter.getText().toString(), 36, "fonts/方正魏碑简体.ttf", Color.BLACK);
+                Integer fontSize;
+                if(!"".equals(etFontSize.getText().toString())){
+                    fontSize = Integer.parseInt(etFontSize.getText().toString());
+                    if(fontSize <= 0){
+                        fontSize = 36;
+                    }
+                }else{
+                    fontSize = 36;
+                }
+
+
+                Bitmap textBitmap = ImageUtil.drawTextToLeftTop(BusinessCardActivity.this, sourBitmap, etLeftTop.getText().toString(), fontSize, "fonts/方正魏碑简体.ttf", Color.BLACK, padding, padding);
+                textBitmap = ImageUtil.drawTextToRightBottom(BusinessCardActivity.this, textBitmap, etRightBottom.getText().toString(), fontSize, "fonts/方正魏碑简体.ttf", Color.BLACK, padding, padding);
+                textBitmap = ImageUtil.drawTextToRightTop(BusinessCardActivity.this, textBitmap, etRightTop.getText().toString(), fontSize,"fonts/方正魏碑简体.ttf", Color.BLACK, padding, padding);
+                textBitmap = ImageUtil.drawTextToLeftBottom(BusinessCardActivity.this, textBitmap, etLeftBottom.getText().toString(), fontSize, "fonts/方正魏碑简体.ttf", Color.BLACK, padding, padding);
+                textBitmap = ImageUtil.drawTextToCenter(BusinessCardActivity.this, textBitmap, etCenter.getText().toString(), fontSize, "fonts/方正魏碑简体.ttf", Color.BLACK);
 
                 mWartermarkImage.setImageBitmap(textBitmap);
             }
@@ -89,6 +116,18 @@ public class BusinessCardActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode == 0x1){
+                if(null != data){
+                    mSourImage.setImageURI(data.getData());
+                }
+            }
+        }
     }
 
     public void saveBitmap(String dirPath, String picName, Bitmap bm) {
