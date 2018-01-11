8e4921124b4fb2a8a978c422ffda88c0a67255a4
simonsundev <simon.sun.dev@hotmail.com>
2018-01-11 20:34:10 +0800

    AlarmReceiver适配Android O。

--- app/src/main/java/com/jeesun/twentyone/widget/AlarmReceiver.java
+++ app/src/main/java/com/jeesun/twentyone/widget/AlarmReceiver.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.jeesun.twentyone.WidgetActivity;
+
 /**
  * Created by simon on 2017/12/30.
  */
@@ -13,9 +15,13 @@ public class AlarmReceiver extends BroadcastReceiver {
     private final static String TAG = AlarmReceiver.class.getName();
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i(TAG, "AlarmReceiver执行onReceive");
-        Intent updateWidgetBgIntent = new Intent(WidgetProvider.ACTION_UPDATE_ALL);
+        Log.i(TAG, "执行onReceive");
+        //参考http://blog.csdn.net/chenshengfa/article/details/71407704
+        //android 8.0 后台限制，如果这样发送隐式广播，receiver将接收不到广播。
+        /*Intent updateWidgetBgIntent = new Intent(WidgetProvider.ACTION_UPDATE_ALL);
         context.sendBroadcast(updateWidgetBgIntent);
-        Log.i(TAG, "广播" + WidgetProvider.ACTION_UPDATE_ALL + "已发送");
+        Log.i(TAG, "广播" + WidgetProvider.ACTION_UPDATE_ALL + "已发送");*/
+        Intent updateClockIntent = new Intent(context, ChangeClockColorReceiver.class);
+        context.sendBroadcast(updateClockIntent);
     }
 }
