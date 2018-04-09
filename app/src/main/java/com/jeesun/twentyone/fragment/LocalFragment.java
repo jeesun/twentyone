470488dca2d81f335c83fb03dae8e3bed9f971de
simonsundev <simon.sun.dev@hotmail.com>
2018-04-10 01:25:16 +0800

    细节调整。

--- app/src/main/java/com/jeesun/twentyone/fragment/LocalFragment.java
+++ app/src/main/java/com/jeesun/twentyone/fragment/LocalFragment.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -52,9 +53,10 @@ public class LocalFragment extends Fragment implements SwipeRefreshLayout.OnRefr
     private static HashMap<Integer, Boolean> isSelected = new HashMap<>();
 
     String dirPath = ContextUtil.picSavePath;
-    @Nullable
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView");
         rootView = inflater.inflate(R.layout.fragment_local, container, false);
 
         swipeRefreshLayout = rootView.findViewById(R.id.grid_swipe_refresh);
